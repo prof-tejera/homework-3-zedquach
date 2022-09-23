f4795bd1c1935ff76062692fc4cc879b9a8a50c0
zedquach <zedquach@users.noreply.github.com>
2022-09-23 01:01:19 +0000

    deploy: d43f88ac99ae451d4a77be7a8fa70819b8058997

--- src/components/Input/Input.js
+++
@@ -1,9 +0,0 @@
-import { Component } from 'react';
-
-class Input extends Component {
-  render() {
-    return <input />;
-  }
-}
-
-export default Input;
--- src/components/Panel/Panel.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Panel extends Component {
-  render() {
-    return <div>{this.props.children}</div>;
-  }
-}
-
-export default Panel;
